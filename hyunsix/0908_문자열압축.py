4fb7205f0a5c3027c03c22028163d7e4701f6809
조현식 <gustlr5043@naver.com>
2021-09-08 23:19:19 +0900

    0908 카카오기출 문자열압축

---
+++ hyunsix/0908_문자열압축.py
@@ -0,0 +1,37 @@
+def solution(s):
+    n = len(s)
+    max_count = 1e10
+    idx = 0
+    if n == 1:
+        return 1
+    for i in range(1, n // 2 + 1):
+        now_pattern = s[:i]
+        count = 1
+        temp_word = ''
+        for j in range(i, len(s), i):
+            target_pattern = s[j:j + i]
+            if now_pattern != target_pattern:
+                if count > 1:
+                    sum_pattern = str(count) + now_pattern
+                    temp_word += sum_pattern
+                else:
+                    temp_word += now_pattern
+
+                now_pattern = target_pattern
+                count = 1
+
+            else:
+                count += 1
+
+        if count > 1:
+
+            sum_pattern = str(count) + now_pattern
+            temp_word += sum_pattern
+
+        else:
+            temp_word += now_pattern
+
+        if max_count > len(temp_word):
+            max_count = len(temp_word)
+
+    return max_count
